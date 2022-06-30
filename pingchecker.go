0d330622b3ace64f21dced0f79fa4621a8c52e6b
Michael Glitzos <michael.glitzos@gmail.com>
2022-06-30 04:27:19 +0000

    correct go workflow

--- pingchecker.go
+++
@@ -1,9 +0,0 @@
-package gohealth
-
-type PingChecker struct{}
-
-func (checker *PingChecker) CheckHealth() HealthCheckResult {
-	result := HealthCheckResult{Service: "ping"}
-	result.Status = UP
-	return result
-}
